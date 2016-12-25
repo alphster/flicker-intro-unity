b67561288079f0385be32bf66620e7872813ed31
Ralph N <ralph@n.com>
2016-12-24 23:38:51 -0800

    Added some beginnings of game state transitions

---
+++ Assets/GameController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+
+    public static GameController Instance
+    {
+        get
+        {
+            return GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+        }
+    }
+
+    private GameState currentGameState;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentGameState = GameState.Start;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void SetState(GameState state)
+    {
+        if (state == GameState.Intro)
+        {
+            if (currentGameState == GameState.Start)
+                MainCamera.Instance.MoveToIntroPosition();
+        }
+        else if (state == GameState.Campfire)
+        {
+            if (currentGameState == GameState.Intro)
+                // something
+                currentGameState = state;
+        }
+
+        currentGameState = state;
+    }
+
+}
+
+public enum GameState
+{
+    Start,
+    Intro,
+    Campfire
+}
---
+++ Assets/MainCamera.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainCamera : MonoBehaviour
+{
+
+    public static MainCamera Instance
+    {
+        get
+        {
+            return GameObject.FindGameObjectWithTag("MainCamera").GetComponent<MainCamera>();
+        }
+    }
+
+    public Vector3 startPosition = new Vector3(0f, .8f, -5.3f);
+    public Vector3 startRotation = new Vector3(2.8f, 0, 0);
+
+    public Vector3 firePosition = new Vector3(0f, 10.25f, -12f);
+    public Vector3 fireRotation = new Vector3(38f, 0, 0);
+
+    // Use this for initialization
+    void Start()
+    {
+        this.transform.position = startPosition;
+        this.transform.eulerAngles = startRotation;
+    }
+
+    public void MoveToIntroPosition()
+    {
+        this.transform.position = firePosition;
+        this.transform.eulerAngles = fireRotation;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
